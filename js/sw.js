4457ea1456be24eea15a8bd62f67a218dc3ead62
fauzanmsc <fauzanmsc@gmail.com>
2026-05-15 16:27:33 +0700

    dsd

--- js/sw.js
+++ js/sw.js
@@ -2,6 +2,10 @@ const CACHE_NAME = 'jef-v1';
 const assets = [
   '/',
   '/index.html',
+  '/dashboard.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/js/manifest.json',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'
 ];
@@ -10,6 +14,21 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
